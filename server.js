4ff3723ff38fc7bd6ea2eadebade20fd97b059a8
quyencao <boy20195@gmail.com>
2019-07-25 09:44:34 +0700

    init commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Success' });
+});
+
+app.post('/webhooks/github', (req, res) => {
+    console.log(res.body);
+});
+
+app.listen(5001, () => console.log('Server is started...'));
